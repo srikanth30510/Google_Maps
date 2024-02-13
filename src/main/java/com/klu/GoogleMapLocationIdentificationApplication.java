629486a07cc8579140325809730e7f2b98a05327
srikanth <srikanth891922@gmail.com>
2024-02-13 21:13:25 +0530

    gm1

---
+++ src/main/java/com/klu/GoogleMapLocationIdentificationApplication.java
@@ -0,0 +1,13 @@
+package com.klu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleMapLocationIdentificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleMapLocationIdentificationApplication.class, args);
+	}
+
+}
